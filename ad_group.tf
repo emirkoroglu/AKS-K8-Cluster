8f4fd973bf05204ebff5117119324ae0a331ac84
emir <emirmails@gmail.com>
2023-02-07 23:58:41 -0500

    module version updated

--- ad_group.tf
+++ ad_group.tf
@@ -1,12 +1,12 @@
 # DevOps group to manage cluster
 resource "azuread_group" "example" {
-	display_name = "DevOps"
-	mail_enabled = true
-	mail_nickname = "ExampleGroup"
-	security_enabled = true
-	types = ["Unified"]
-	# members = [
-	# azuread_user.example.object_id,
-     # /* more users */
-	# ]
+  display_name     = "DevOps"
+  mail_enabled     = true
+  mail_nickname    = "ExampleGroup"
+  security_enabled = true
+  types            = ["Unified"]
+  # members = [
+  # azuread_user.example.object_id,
+  # /* more users */
+  # ]
 }
