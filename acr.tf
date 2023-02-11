ea7ca3457ff41f350e17461d93e9e8d52fff8192
emir <emirmails@gmail.com>
2023-02-11 17:13:28 -0500

    azure container  services added attached to aks

---
+++ acr.tf
@@ -0,0 +1,21 @@
+resource "azurerm_resource_group" "dev-env-resource-group" {
+  name     = "dev-env-resource-group"
+  location = "eastus"
+}
+# terraform import azurerm_resource_group.dev-env-resource-group /subscriptions/50d2bf63-13d4-467a-beae-283dd8d9727e/resourceGroups/dev-env-resource-group
+
+resource "azurerm_container_registry" "DevcontainerRegistry33dev2" {
+  name                = "DevcontainerRegistry33dev2"
+  resource_group_name = azurerm_resource_group.dev-env-resource-group.name
+  location            = azurerm_resource_group.dev-env-resource-group.location
+  sku                 = "Premium"
+}
+
+resource "azurerm_role_assignment" "example" {
+  principal_id                     = "cb603d69-1252-4049-8429-6a6f8d97da49"
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.DevcontainerRegistry33dev2.id
+  skip_service_principal_aad_check = true
+}
+
+
